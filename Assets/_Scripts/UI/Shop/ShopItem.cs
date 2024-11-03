a6768047f6082fd8a9d0745daaacddf7767cc05d
arboos <151074202+arboos@users.noreply.github.com>
2024-11-03 14:03:07 +0300

    old map

---
+++ Assets/_Scripts/UI/Shop/ShopItem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ShopItem : ScriptableObject
+{
+    [Header("INDEX")] 
+    public int index;
+    
+    [Header("Economic")]
+    public bool boughtYet = false;
+    public int price;
+
+    [Header("Settings")]
+    public Sprite icon;
+    public string name;
+    public string info;
+    public string characteristics;
+    
+}
